63d8064d5f3b00677c5a2b90cd2cb3f615dc9e1f
Aakash <aakash_shakya@hotmail.com>
2022-01-26 11:02:42 +0545

    PHP basiscs changes in certain files

---
+++ unit-2/php-constant.php
@@ -0,0 +1,12 @@
+<?php
+    //General practice is define constant variable in capital letters
+    define('PI', 3.1415, true);
+    define('HOME_URL', 'http://localhost/BCA-Scripting-Language/unit-2/');
+    define('cars', array('BMW', 'FORD', 'Mercedez'));
+
+    echo pi;
+    echo "<br>";
+    echo HOME_URL;
+    echo "<br>";
+    print_r(cars);
+?>
